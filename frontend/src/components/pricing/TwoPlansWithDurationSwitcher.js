554b372d593696199085ac3668124f606aaf83eb
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2021-10-04 12:20:45 +0800

    [BUG] tried implementing modal, placement is wrong

--- frontend/src/components/pricing/TwoPlansWithDurationSwitcher.js
+++ frontend/src/components/pricing/TwoPlansWithDurationSwitcher.js
@@ -13,6 +13,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { Form } from "react-bootstrap";
 import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
+import { Containera } from './Container';
 import "./styles.css";
 const HeaderContainer = tw.div`w-full flex flex-col items-center`;
 const Subheading = tw(SubheadingBase)`mb-4`;
@@ -327,6 +328,12 @@ export default ({
       </form>
     );
   };
+  const triggerText = 'Open form';
+  const onSubmit = (event) => {
+    event.preventDefault(event);
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+  };
   return (
     <Container>
       <ContentWithPaddingXl>
@@ -337,8 +344,9 @@ export default ({
           <Elements stripe={stripePromise}>
             <Form />
             <CheckoutForm />
+            
           </Elements>
-          
+          <Containera triggerText={triggerText} onSubmit={onSubmit} />
           <PlanDurationSwitcher>
             {planDurations.map((planDuration, index) => (
               <SwitchButton active={activeDurationIndex === index} key={index} onClick={() => setActiveDurationIndex(index)}>{planDuration.switcherText}</SwitchButton>
