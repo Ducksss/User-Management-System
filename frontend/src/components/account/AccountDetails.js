7b9e6d8632ac0088a01cbc5e085124ee8c40805c
lekekbot <alim97460@gmail.com>
2021-10-10 16:52:36 +0800

    [FIX] css fixes to account details

--- frontend/src/components/account/AccountDetails.js
+++ frontend/src/components/account/AccountDetails.js
@@ -8,9 +8,9 @@ import axios from "axios";
 import Slider from './Slider';
 import Swal from "sweetalert2";
 const MainContent = tw.div`mt-12 flex flex-col items-center w-full`;
-const AccountRow = tw.div` grid grid-rows-3 w-11/12`
-const GridRow = tw.div`flex`
-const DetailRow = tw.div`border rounded-lg border-gray-400  w-8/12 `
+const AccountRow = tw.div` grid w-11/12`
+const GridRow = tw.div`flex flex-wrap flex-row`
+const DetailRow = tw.div`border rounded-lg border-gray-400  w-8/12 my-3`
 const LeftHeader = tw.div` text-xl font-black w-4/12`
 
 const InfoRow = tw.div`flex flex-row border-b border-gray-400 px-3 py-5`
@@ -68,7 +68,7 @@ export default function AccountDetails(location) {
 
     const [showModal, setShowModal] = useState(false)
     const [sendData, setsendData] = useState('')
-    const [cancelled, setCancelled] = useState(false);
+
     const getUserDetails = () => {
         //personal info has 3 keys, so have to loop to get all of it to display
         if (data) {
@@ -145,40 +145,34 @@ export default function AccountDetails(location) {
         fetchData();
     }, []);
 
-    if (!subscriptions) {
-        return '';
-    }
+    if (!subscriptions) return '';
+
     const AccountSubscription = ({ subscription }) => {
-        return (
-            <GridRow>
-                
+        return (              
             <DetailRow>
-                <hr />
                 <InfoRow>
                     <Header>Subscription ID:</Header>
-                    <Content>
-                        {subscription.id}
-                    </Content>
+                    <Content>{subscription.id}</Content>
                 </InfoRow>
 
                 <InfoRow>
-                    <Header>Status:</Header><Content> {subscription.status}</Content>
+                    <Header>Status:</Header>
+                    <Content> {subscription.status}</Content>
                 </InfoRow>
 
                 <InfoRow>
-                    <Header>Card last4:</Header><Content> {subscription.default_payment_method?.card?.last4}</Content>
+                    <Header>Card last4:</Header>
+                    <Content>{subscription.default_payment_method?.card?.last4}</Content>
                 </InfoRow>
 
                 <InfoRowLast>
-                    <Header>Current period end:</Header><Content> {(new Date(subscription.current_period_end * 1000).toString())}</Content>
+                    <Header>Current period end:</Header>
+                    <Content> {(new Date(subscription.current_period_end * 1000).toString())}</Content>
                 </InfoRowLast>
 
                 {/* <Link to={{pathname: '/change-plan', state: {subscription: subscription.id }}}>Change plan</Link><br /> */}
                 <button onClick={(e) => { handleClick(e, subscription.id) }}>Cancel</button>
-
             </DetailRow>
-            </GridRow>
-
         )
     }
     return (
@@ -213,18 +207,20 @@ export default function AccountDetails(location) {
                     </DetailRow>
                 </GridRow>
                 <Line />
-                {/* any other sections here */}
-                </AccountRow>
-
-                <AccountRow >
-                    
-                <LeftHeader>Subscriptions</LeftHeader>
-                    {subscriptions.map(s => {
-                        return <AccountSubscription key={s.id} subscription={s} />
+                <GridRow >
+                    {subscriptions.map((s, i) => {
+                        return (
+                        <>
+                            <LeftHeader>{i == 0 ? 'Subscriptions' : ''}</LeftHeader>
+                            <AccountSubscription key={s.id} subscription={s} />
+                        </>
+                        )
                     })}
 
+                </GridRow>
+                {/* any other sections here */}
                 </AccountRow>
-
+                
             {/* slider */}
             <Slider show={showModal} hide={setShowModal} data={sendData} getData={e => newdata(e)} />
         </MainContent>
