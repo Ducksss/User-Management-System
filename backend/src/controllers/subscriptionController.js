c762de4da683e49ff2d084882aefd318505e8461
ZeInhumane <69852169+ZeInhumane@users.noreply.github.com>
2021-10-24 20:44:18 +0800

    [CODE] Additional stuff for webhook

--- backend/src/controllers/subscriptionController.js
+++ backend/src/controllers/subscriptionController.js
@@ -55,8 +55,8 @@ exports.createSubscription = async (req, res, next) => {
             payment_behavior: 'default_incomplete',
             expand: ['latest_invoice.payment_intent'],
         });
-        
-        res.status(200).send(codes(200, null,{
+
+        res.status(200).send(codes(200, null, {
             subscriptionId: subscription.id,
             clientSecret: subscription.latest_invoice.payment_intent.client_secret,
         }));
@@ -74,8 +74,8 @@ exports.cancelSubscription = async (req, res, next) => {
         const deletedSubscription = await stripe.subscriptions.del(
             req.body.subscriptionId
         );
-        res.status(200).send(codes(200, null,{
-            subscription: deletedSubscription 
+        res.status(200).send(codes(200, null, {
+            subscription: deletedSubscription
         }));
     } catch (error) {
         return res.status(400).send(codes(400));
@@ -99,21 +99,21 @@ exports.subscriptions = async (req, res, next) => {
 };
 exports.webhook = async (req, res, next) => {
     // Retrieve the event by verifying the signature using the raw body and secret.
-    let event =req.body;
-
+    let event;
+    console.log(process.env.STRIPE_WEBHOOK_SECRET)
     try {
-      event = stripe.webhooks.constructEvent(
-        req.body,
-        req.header('Stripe-Signature'),
-        process.env.STRIPE_WEBHOOK_SECRET
-      );
+        event = stripe.webhooks.constructEvent(
+            req.rawBody,
+            req.headers['stripe-signature'],
+            process.env.STRIPE_WEBHOOK_SECRET
+        );
     } catch (err) {
-      console.log(err);
-      console.log(`⚠️  Webhook signature verification failed.`);
-      console.log(
-        `⚠️  Check the env file and enter the correct webhook secret.`
-      );
-      return res.sendStatus(400);
+        console.log(err);
+        console.log(`⚠️  Webhook signature verification failed.`);
+        console.log(
+            `⚠️  Check the env file and enter the correct webhook secret.`
+        );
+        return res.status(400).send(codes(400));
     }
 
     // Extract the object from the event.
@@ -126,57 +126,96 @@ exports.webhook = async (req, res, next) => {
     console.log(event.type)
     console.log("event type")
     switch (event.type) {
-      case 'invoice.payment_succeeded':
-        if(dataObject['billing_reason'] == 'subscription_create') {
-          // The subscription automatically activates after successful payment
-          // Set the payment method used to pay the first invoice
-          // as the default payment method for that subscription
-          const subscription_id = dataObject['subscription']
-          const payment_intent_id = dataObject['payment_intent']
+        case 'invoice.payment_succeeded':
+            if (dataObject['billing_reason'] == 'subscription_create') {
+                // The subscription automatically activates after successful payment
+                // Set the payment method used to pay the first invoice
+                // as the default payment method for that subscription
+                const subscription_id = dataObject['subscription']
+                const payment_intent_id = dataObject['payment_intent']
 
-          // Retrieve the payment intent used to pay the subscription
-          const payment_intent = await stripe.paymentIntents.retrieve(payment_intent_id);
+                // Retrieve the payment intent used to pay the subscription
+                const payment_intent = await stripe.paymentIntents.retrieve(payment_intent_id);
 
-          const subscription = await stripe.subscriptions.update(
-            subscription_id,
-            {
-              default_payment_method: payment_intent.payment_method,
-            },
-          );
+                const subscription = await stripe.subscriptions.update(
+                    subscription_id,
+                    {
+                        default_payment_method: payment_intent.payment_method,
+                    },
+                );
 
-          console.log("Default payment method set for subscription:" + payment_intent.payment_method);
-        };
+                console.log("Default payment method set for subscription:" + payment_intent.payment_method);
+            };
 
-        break;
-      case 'invoice.payment_failed':
-        console.log(event.data.object)
-        // If the payment fails or the customer does not have a valid payment method,
-        //  an invoice.payment_failed event is sent, the subscription becomes past_due.
-        // Use this webhook to notify your user that their payment has
-        // failed and to retrieve new card details.
-        break;
-      case 'invoice.finalized':
-        console.log(event.data.object)
-        // If you want to manually send out invoices to your customers
-        // or store them locally to reference to avoid hitting Stripe rate limits.
-        break;
-      case 'customer.subscription.deleted':
-        if (event.request != null) {
-          console.log(event.data.object)
-          // handle a subscription cancelled by your request
-          // from above.
-        } else {
-          console.log(event.data.object)
-          // handle subscription cancelled automatically based
-          // upon your subscription settings.
-        }
-        break;
-      case 'customer.subscription.trial_will_end':
-        console.log(event.data.object)
-        // Send notification to your user that the trial will end
-        break;
-      default:
-      // Unexpected event type
+            break;
+        case 'invoice.payment_failed':
+            console.log(event.data.object)
+            const invoice = event.data.object;
+            const subscriptionID = invoice.subscription;
+            // If the payment fails due to card authentication required,
+            // an invoice.payment_failed event is sent, the subscription becomes past_due.
+            // Use this webhook to notify user that their payment has
+            // failed and to retrieve new card details.
+
+            const amount = parseFloat(invoice.amount_due / 100).toFixed(2);
+            const currentPeriodStart = dayjs(invoice.period_start * 1000).toDate();
+            const currentPeriodEnd = dayjs(invoice.period_end * 1000).toDate();
+
+            // Check if invoice exists in our Database already
+            const invoiceExists = await findInvoice(invoice.id);
+            if (invoiceExists) {
+                // Update invoice
+                await updateInvoice(invoice.id, {
+                    stripe_payment_intent_status: 'requires_action',
+                    stripe_reference_number: invoice.number,
+                    amount,
+                    balance: parseFloat(invoice.ending_balance / 100).toFixed(2),
+                    fk_stripe_subscription_id: subscriptionID,
+                });
+            } else {
+                const paymentIntentID = invoice.payment_intent;
+                const paymentIntent = await findPaymentIntent(paymentIntentID);
+                const clientSecret = paymentIntent.client_secret;
+
+                // Insert invoice
+                await createInvoice({
+                    stripe_invoice_id: invoice.id,
+                    stripe_reference_number: invoice.number,
+                    stripe_payment_intent_id: paymentIntentID,
+                    stripe_client_secret: clientSecret,
+                    stripe_payment_intent_status: 'requires_action',
+                    amount,
+                    balance: parseFloat(invoice.ending_balance / 100).toFixed(2),
+                    fk_stripe_subscription_id: subscriptionID,
+                });
+            }
+            // If the payment fails or the customer does not have a valid payment method,
+            //  an invoice.payment_failed event is sent, the subscription becomes past_due.
+            // Use this webhook to notify your user that their payment has
+            // failed and to retrieve new card details.
+            break;
+        case 'invoice.finalized':
+            console.log(event.data.object)
+            // If you want to manually send out invoices to your customers
+            // or store them locally to reference to avoid hitting Stripe rate limits.
+            break;
+        case 'customer.subscription.deleted':
+            if (event.request != null) {
+                console.log(event.data.object)
+                // handle a subscription cancelled by your request
+                // from above.
+            } else {
+                console.log(event.data.object)
+                // handle subscription cancelled automatically based
+                // upon your subscription settings.
+            }
+            break;
+        case 'customer.subscription.trial_will_end':
+            console.log(event.data.object)
+            // Send notification to your user that the trial will end
+            break;
+        default:
+        // Unexpected event type
     }
     res.sendStatus(200);
 };
--- backend/src/routes/subscriptionRoute.js
+++ backend/src/routes/subscriptionRoute.js
@@ -1,9 +1,9 @@
 const subscriptionController = require('../controllers/subscriptionController');
 
 exports.route = router => {
-    router.post('/api/u/subscription/webhook' , subscriptionController.webhook)
+    router.post('/webhook' , subscriptionController.webhook)
     router.get('/api/u/subscription/config', subscriptionController.config);
-    router.get('/api/u/subscription/create_customer', subscriptionController.createCustomer);
+    router.get('/api/u/subscription/create-customer', subscriptionController.createCustomer);
     router.post('/api/u/subscription/create', subscriptionController.createSubscription);
     router.post('/api/u/subscription/cancel', subscriptionController.cancelSubscription);
     router.get('/api/u/subscription/subscriptions', subscriptionController.subscriptions);
